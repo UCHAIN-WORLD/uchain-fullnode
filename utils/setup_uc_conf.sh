86c7fe8cdded5a78f8009d3c2d77974fdb06e114
yangguanglu <416841146@qq.com>
2018-11-07 18:02:53 +0800

    modify 5678 port to 5679 to avoid port conflict

--- utils/setup_uc_conf.sh
+++ utils/setup_uc_conf.sh
@@ -8,5 +8,5 @@ mkdir -p ~/.UChain/
 if [ ! -f ${TARGET} ]; then
     cp ../etc/uc.conf ${TARGET}
     sed -i 's/127.0.0.1:8707/0.0.0.0:8707/g' ${TARGET}
-    sed -i "/\[network\]/a self=${MYIP}:5678" ${TARGET}
+    sed -i "/\[network\]/a self=${MYIP}:5682" ${TARGET}
 fi
