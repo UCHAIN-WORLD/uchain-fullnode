fca3f08a8fdf8300278eb0d25d7c5f3460d6c49f
o-u-p <y758033884@gmail.com>
2018-12-11 09:47:36 +0800

    match linux

--- include/UChainService/api/restful/compat/string_view.h
+++ include/UChainService/api/restful/compat/string_view.h
@@ -21,6 +21,11 @@
 #define UC___STRING_VIEW__
 
 #ifdef _WIN32
+#include <boost/utility/string_view_fwd.hpp>
+#include <boost/utility/string_view.hpp>
+#define string_view boost::string_view
+
+#else
 
 #include <experimental/string_view>
 namespace mgbubble{
