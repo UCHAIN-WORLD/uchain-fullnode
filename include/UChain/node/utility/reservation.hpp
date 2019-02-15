11fe09d3e5c40e3f8495b09d53218ee5c716f2e7
yangguanglu <416841146@qq.com>
2019-02-15 10:25:57 +0800

    node utility

--- include/UChain/node/utility/reservation.hpp
+++ include/UChain/node/utility/reservation.hpp
@@ -32,22 +32,24 @@
 #include <UChain/node/define.hpp>
 #include <UChain/node/utility/performance.hpp>
 
-namespace libbitcoin {
-namespace node {
+namespace libbitcoin
+{
+namespace node
+{
 
 class reservations;
 
 // Class to manage hashes during sync, thread safe.
 class BCN_API reservation
-  : public enable_shared_from_base<reservation>
+    : public enable_shared_from_base<reservation>
 {
-public:
+  public:
     typedef std::shared_ptr<reservation> ptr;
     typedef std::vector<reservation::ptr> list;
 
     /// Construct a block reservation with the specified identifier.
-    reservation(reservations& reservations, size_t slot,
-        uint32_t block_timeout_seconds);
+    reservation(reservations &reservations, size_t slot,
+                uint32_t block_timeout_seconds);
 
     /// Ensure there are no remaining reserved hashes.
     ~reservation();
@@ -77,17 +79,17 @@ public:
     performance rate() const;
 
     /// The current cached average block import rate excluding import time.
-    void set_rate(const performance& rate);
+    void set_rate(const performance &rate);
 
     /// The block data request message for the outstanding block hashes.
     /// Set new if the preceding request was unsuccessful or discarded.
     message::get_data request(bool new_channel);
 
     /// Add the block hash to the reservation.
-    void insert(const config::checkpoint& checkpoint);
+    void insert(const config::checkpoint &checkpoint);
 
     /// Add the block hash to the reservation.
-    void insert(const hash_digest& hash, size_t height);
+    void insert(const hash_digest &hash, size_t height);
 
     /// Add to the blockchain, with height determined by the reservation.
     void import(chain::block::ptr block);
@@ -101,7 +103,7 @@ public:
     /// If not stopped and if empty try to get more hashes.
     void populate();
 
-protected:
+  protected:
     // Accessor for testability.
     bool pending() const;
 
@@ -114,7 +116,7 @@ protected:
     // Isolation of side effect to enable unit testing.
     virtual std::chrono::high_resolution_clock::time_point now() const;
 
-private:
+  private:
     typedef struct
     {
         size_t events;
@@ -127,19 +129,20 @@ private:
     // A bidirection map is used for efficient hash and height retrieval.
     typedef boost::bimaps::bimap<
         boost::bimaps::unordered_set_of<hash_digest>,
-        boost::bimaps::set_of<uint32_t>> hash_heights;
+        boost::bimaps::set_of<uint32_t>>
+        hash_heights;
 
     // Return rate history to startup state.
     void clear_history();
 
     // Get the height of the block hash, remove and return true if it is found.
-    bool find_height_and_erase(const hash_digest& hash, uint32_t& out_height);
+    bool find_height_and_erase(const hash_digest &hash, uint32_t &out_height);
 
     // Update rate history to reflect an additional block of the given size.
-    void update_rate(size_t events, const std::chrono::microseconds& database);
+    void update_rate(size_t events, const std::chrono::microseconds &database);
 
     // Thread safe.
-    reservations& reservations_;
+    reservations &reservations_;
 
     // Protected by rate mutex.
     performance rate_;
@@ -167,4 +170,3 @@ private:
 } // namespace libbitcoin
 
 #endif
-
