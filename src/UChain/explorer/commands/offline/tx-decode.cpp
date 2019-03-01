5d18a87eca2df85eeaf35b8ccc326fcfbc477735
yangguanglu <504794637@qq.com>
2019-03-01 14:35:48 +0800

    offline commands

--- src/UChain/explorer/commands/offline/tx-decode.cpp
+++ src/UChain/explorer/commands/offline/tx-decode.cpp
@@ -26,17 +26,19 @@
 #include <UChain/explorer/json_helper.hpp>
 #include <UChain/explorer/utility.hpp>
 
-
-namespace libbitcoin {
-namespace explorer {
-namespace commands {
+namespace libbitcoin
+{
+namespace explorer
+{
+namespace commands
+{
 using namespace bc::explorer::config;
 
-console_result tx_decode::invoke(std::ostream& output, std::ostream& error)
+console_result tx_decode::invoke(std::ostream &output, std::ostream &error)
 {
     // Bound parameters.
-    const auto& encoding = get_format_option();
-    const auto& transaction = get_transaction_argument();
+    const auto &encoding = get_format_option();
+    const auto &transaction = get_transaction_argument();
 
     // This enables json-style array formatting.
     const auto json = encoding == encoding_engine::json;
