c1eff027edc7cba3266628bf4be8a46e6abd574e
yangguanglu <416841146@qq.com>
2019-03-22 16:36:58 +0800

    importkeyfile ,importwallet ,registercandidate

--- src/UChainService/api/command/commands/importkeyfile.cpp
+++ src/UChainService/api/command/commands/importkeyfile.cpp
@@ -26,20 +26,24 @@
 #include <UChain/bitcoin/formats/base_64.hpp>
 #include <cryptojs/cryptojs_impl.h>
 
-namespace libbitcoin {
-namespace explorer {
-namespace commands {
+namespace libbitcoin
+{
+namespace explorer
+{
+namespace commands
+{
 namespace fs = boost::filesystem;
 using namespace bc::explorer::config;
 
 /************************ importkeyfile *************************/
 
-console_result importkeyfile::invoke(Json::Value& jv_output,
-    libbitcoin::server::server_node& node)
+console_result importkeyfile::invoke(Json::Value &jv_output,
+                                     libbitcoin::server::server_node &node)
 {
-    auto& blockchain = node.chain_impl();
+    auto &blockchain = node.chain_impl();
 
-    if (blockchain.is_wallet_exist(auth_.name)){
+    if (blockchain.is_wallet_exist(auth_.name))
+    {
         throw wallet_existed_exception{auth_.name + " wallet already exist"};
     }
 
@@ -50,9 +54,12 @@ console_result importkeyfile::invoke(Json::Value& jv_output,
     acc->set_passwd(auth_.auth);
 
     std::string file_content;
-    if (option_.content != "") {
+    if (option_.content != "")
+    {
         file_content = option_.content;
-    } else {
+    }
+    else
+    {
         fs::file_status status = fs::status(option_.file);
         if (!fs::exists(status)) // not process filesystem exception here
             throw argument_legality_exception{option_.file.string() + std::string(" not exist.")};
@@ -69,27 +76,32 @@ console_result importkeyfile::invoke(Json::Value& jv_output,
 
     Json::Reader reader;
     Json::Value file_root;
-    if (file_content.length() && (file_content[0] == '{') && reader.parse(file_content, file_root)){
+    if (file_content.length() && (file_content[0] == '{') && reader.parse(file_content, file_root))
+    {
         //mnemonic
         {
             const std::string encoded_mnemonic = file_root["mnemonic"].asString();
             data_chunk decoded_mnemonic;
-            if (!libbitcoin::decode_base64(decoded_mnemonic, encoded_mnemonic)) {
+            if (!libbitcoin::decode_base64(decoded_mnemonic, encoded_mnemonic))
+            {
                 throw encode_exception{encoded_mnemonic + std::string(" invalid for decode_base64.")};
             }
             std::string mnemonic;
-            if (!cryptojs::decrypt(decoded_mnemonic, auth_.auth, mnemonic)) {
+            if (!cryptojs::decrypt(decoded_mnemonic, auth_.auth, mnemonic))
+            {
                 throw encode_exception{std::string("Failed to decrypt mnemonic with password.")};
             }
 
-            if ((mnemonic[0] != '"') || (mnemonic[mnemonic.size()-1] != '"')) {
+            if ((mnemonic[0] != '"') || (mnemonic[mnemonic.size() - 1] != '"'))
+            {
                 throw encode_exception{std::string("decrypted mnemonic format error.")};
             }
 
-            acc->set_mnemonic(mnemonic.substr(1, mnemonic.size()-2), auth_.auth);
+            acc->set_mnemonic(mnemonic.substr(1, mnemonic.size() - 2), auth_.auth);
         }
 
-        if (blockchain.store_wallet(acc) != console_result::okay) {
+        if (blockchain.store_wallet(acc) != console_result::okay)
+        {
             throw address_generate_exception{std::string("Failed to store wallet.")};
         }
 
@@ -102,7 +114,8 @@ console_result importkeyfile::invoke(Json::Value& jv_output,
             const char *cmds2[]{"addaddress", auth_.name.c_str(), auth_.auth.c_str(), "--number",
                                 address_count.c_str()};
 
-            if (dispatch_command(5, cmds2, jv_temp, node, get_api_version()) != console_result::okay) {
+            if (dispatch_command(5, cmds2, jv_temp, node, get_api_version()) != console_result::okay)
+            {
                 throw address_generate_exception{std::string("Failed to generate address.")};
             }
         }
@@ -111,7 +124,8 @@ console_result importkeyfile::invoke(Json::Value& jv_output,
         {
             const auto &multisigs = file_root["multisigs"];
             const int multisigs_size = multisigs.size();
-            for (int i=0; i < multisigs_size; ++i) {
+            for (int i = 0; i < multisigs_size; ++i)
+            {
                 std::string d = multisigs[i]["d"].asString();
                 std::string m = multisigs[i]["m"].asString();
                 std::string n = multisigs[i]["n"].asString();
@@ -120,7 +134,8 @@ console_result importkeyfile::invoke(Json::Value& jv_output,
                 std::vector<std::string> vec_k;
 
                 const auto &k = multisigs[i]["k"];
-                for (unsigned int j=0; j < k.size(); ++j) {
+                for (unsigned int j = 0; j < k.size(); ++j)
+                {
                     std::string d = k[j].asString();
                     vec_k.push_back(d);
                 }
@@ -128,17 +143,18 @@ console_result importkeyfile::invoke(Json::Value& jv_output,
                 // get n new sub-address
                 Json::Value jv_temp;
                 std::vector<const char *> vec_cmds = {"createmultisigaddress", auth_.name.c_str(), auth_.auth.c_str(),
-                                    "-s", s.c_str(),
-                                    "-m", m.c_str(),
-                                    "-n", n.c_str(),
-                                    "-d", d.c_str()
-                };
-                for (const auto& s : vec_k) {
+                                                      "-s", s.c_str(),
+                                                      "-m", m.c_str(),
+                                                      "-n", n.c_str(),
+                                                      "-d", d.c_str()};
+                for (const auto &s : vec_k)
+                {
                     vec_cmds.push_back("-k");
                     vec_cmds.push_back(s.c_str());
                 }
 
-                if (dispatch_command(vec_cmds.size(), vec_cmds.data(), jv_temp, node, get_api_version()) != console_result::okay) {
+                if (dispatch_command(vec_cmds.size(), vec_cmds.data(), jv_temp, node, get_api_version()) != console_result::okay)
+                {
                     throw address_generate_exception{std::string("Failed to generate address.")};
                 }
             }
@@ -146,17 +162,19 @@ console_result importkeyfile::invoke(Json::Value& jv_output,
 
         Json::Value jv_temp;
         std::vector<const char *> vec_cmds = {"showaddresses", auth_.name.c_str(), auth_.auth.c_str()};
-        if (dispatch_command(vec_cmds.size(), vec_cmds.data(), jv_temp, node, get_api_version()) != console_result::okay) {
+        if (dispatch_command(vec_cmds.size(), vec_cmds.data(), jv_temp, node, get_api_version()) != console_result::okay)
+        {
             throw wallet_address_get_exception{std::string("Failed to list wallet addresses.")};
         }
 
-        auto& root = jv_output;
+        auto &root = jv_output;
         config::json_helper::wallet_info acc(auth_.name, "", jv_temp);
         root = config::json_helper(get_api_version()).prop_list(acc);
 
         return console_result::okay;
     }
-    else {
+    else
+    {
         wallet_info all_info(blockchain, auth_.auth);
         std::stringstream ss(file_content);
         // decrypt wallet info file first
@@ -172,18 +190,21 @@ console_result importkeyfile::invoke(Json::Value& jv_output,
         // store wallet info to db
         all_info.store(name, auth_.auth);
 
-        auto& root = jv_output;
+        auto &root = jv_output;
         root["name"] = name;
 
-        if (get_api_version() == 1) {
+        if (get_api_version() == 1)
+        {
             root["address-count"] += acc.get_hd_index();
             root["unissued-token-count"] += all_info.get_wallet_token().size();
         }
-        else if (get_api_version() <= 2) {
+        else if (get_api_version() <= 2)
+        {
             root["address-count"] = acc.get_hd_index();
             root["unissued-token-count"] = static_cast<uint64_t>(all_info.get_wallet_token().size());
         }
-        else {
+        else
+        {
             root["address_count"] = acc.get_hd_index();
             root["unissued_token_count"] = static_cast<uint64_t>(all_info.get_wallet_token().size());
         }
@@ -192,8 +213,6 @@ console_result importkeyfile::invoke(Json::Value& jv_output,
     }
 }
 
-
 } // namespace commands
 } // namespace explorer
 } // namespace libbitcoin
-
