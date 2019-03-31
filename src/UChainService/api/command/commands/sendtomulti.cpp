9dadc2a38020823864f2d35659b72eddfcef3201
yangguanglu <416841146@qq.com>
2019-03-31 18:26:11 +0800

    sendtokento sendtomulti

--- src/UChainService/api/command/commands/sendtomulti.cpp
+++ src/UChainService/api/command/commands/sendtomulti.cpp
@@ -26,30 +26,34 @@
 #include <UChainService/api/command/exception.hpp>
 #include <UChainService/api/command/base_helper.hpp>
 
-namespace libbitcoin {
-namespace explorer {
-namespace commands {
-
-
-console_result sendtomulti::invoke (Json::Value& jv_output,
-    libbitcoin::server::server_node& node)
+namespace libbitcoin
 {
-    auto& blockchain = node.chain_impl();
+namespace explorer
+{
+namespace commands
+{
+
+console_result sendtomulti::invoke(Json::Value &jv_output,
+                                   libbitcoin::server::server_node &node)
+{
+    auto &blockchain = node.chain_impl();
     blockchain.is_wallet_passwd_valid(auth_.name, auth_.auth);
 
     // receiver
     std::vector<receiver_record> receiver;
 
-    for (auto& each : argument_.receivers) {
+    for (auto &each : argument_.receivers)
+    {
         colon_delimited2_item<std::string, uint64_t> item(each);
 
         asset attach;
         std::string address = get_address(item.first(), attach, false, blockchain);
-        if (item.second() <= 0) {
+        if (item.second() <= 0)
+        {
             throw argument_legality_exception("invalid amount parameter for " + item.first());
         }
 
-        receiver.push_back({address,"", item.second(), 0, utxo_attach_type::ucn, attach});
+        receiver.push_back({address, "", item.second(), 0, utxo_attach_type::ucn, attach});
     }
 
     // change address
@@ -57,23 +61,21 @@ console_result sendtomulti::invoke (Json::Value& jv_output,
     //should own the address
     if (!change_address.empty() && !blockchain.get_wallet_address(auth_.name, change_address))
         throw wallet_authority_exception{"change address not belongs to you."};
-        
+
     auto send_helper = sending_ucn(*this, blockchain,
-        std::move(auth_.name), std::move(auth_.auth),
-        "", std::move(receiver),
-        std::move(change_address), option_.fee);
+                                   std::move(auth_.name), std::move(auth_.auth),
+                                   "", std::move(receiver),
+                                   std::move(change_address), option_.fee);
 
     send_helper.exec();
 
     // json output
     auto tx = send_helper.get_transaction();
-    jv_output =  config::json_helper(get_api_version()).prop_tree(tx, true);
+    jv_output = config::json_helper(get_api_version()).prop_tree(tx, true);
 
     return console_result::okay;
 }
 
-
 } // namespace commands
 } // namespace explorer
 } // namespace libbitcoin
-
