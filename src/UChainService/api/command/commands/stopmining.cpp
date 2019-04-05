5f4b49cfa7612147ef257230ac5cb235196b93ab
yangguanglu <416841146@qq.com>
2019-04-05 16:26:51 +0800

    startmining stopmining

--- src/UChainService/api/command/commands/stopmining.cpp
+++ src/UChainService/api/command/commands/stopmining.cpp
@@ -18,42 +18,44 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-
 #include <jsoncpp/json/json.h>
 #include <UChainService/api/command/node_method_wrapper.hpp>
 #include <UChainService/api/command/commands/stopmining.hpp>
 #include <UChainService/api/command/command_extension_func.hpp>
 #include <UChainService/api/command/exception.hpp>
 
-namespace libbitcoin {
-namespace explorer {
-namespace commands {
-
+namespace libbitcoin
+{
+namespace explorer
+{
+namespace commands
+{
 
 /************************ stopmining *************************/
 
-console_result stopmining::invoke(Json::Value& jv_output,
-    libbitcoin::server::server_node& node)
+console_result stopmining::invoke(Json::Value &jv_output,
+                                  libbitcoin::server::server_node &node)
 {
-    auto& blockchain = node.chain_impl();
-    auto& miner = node.miner();
+    auto &blockchain = node.chain_impl();
+    auto &miner = node.miner();
     blockchain.is_wallet_passwd_valid(auth_.name, auth_.auth);
-    if(!administrator_required_checker(node, auth_.name, auth_.auth) && \
+    if (!administrator_required_checker(node, auth_.name, auth_.auth) &&
         !blockchain.get_wallet_address(auth_.name, miner.get_miner_address()))
-            throw wallet_authority_exception{"not the miner wallet."};
+        throw wallet_authority_exception{"not the miner wallet."};
     auto ret = miner.stop();
 
-    if (ret) {
+    if (ret)
+    {
         jv_output = "signal STOP sent.";
-    } else {
+    }
+    else
+    {
         throw unknown_error_exception{"mining stop got error."};
     }
 
     return console_result::okay;
 }
 
-
 } // namespace commands
 } // namespace explorer
 } // namespace libbitcoin
-
