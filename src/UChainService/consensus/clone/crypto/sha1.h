c9bf8e1775cf645353e1f3b8f670586259bd83fc
yangguanglu <416841146@qq.com>
2019-04-12 14:30:20 +0800

    sha1

--- src/UChainService/consensus/clone/crypto/sha1.h
+++ src/UChainService/consensus/clone/crypto/sha1.h
@@ -11,18 +11,18 @@
 /** A hasher class for SHA1. */
 class CSHA1
 {
-private:
+  private:
     uint32_t s[5];
     unsigned char buf[64];
     size_t bytes;
 
-public:
+  public:
     static const size_t OUTPUT_SIZE = 20;
 
     CSHA1();
-    CSHA1& Write(const unsigned char* data, size_t len);
+    CSHA1 &Write(const unsigned char *data, size_t len);
     void Finalize(unsigned char hash[OUTPUT_SIZE]);
-    CSHA1& Reset();
+    CSHA1 &Reset();
 };
 
 #endif // BITCOIN_CRYPTO_SHA1_H
