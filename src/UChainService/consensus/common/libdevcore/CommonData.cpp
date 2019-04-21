aa5fb027f6e53df04f329315b92131586d0c82aa
yangguanglu <416841146@qq.com>
2019-04-21 13:53:35 +0800

    CommonData

--- src/UChainService/consensus/common/libdevcore/CommonData.cpp
+++ src/UChainService/consensus/common/libdevcore/CommonData.cpp
@@ -25,7 +25,7 @@
 
 #if defined(_MSC_VER)
 #pragma warning(push)
-#pragma warning(disable:4724) // potential mod by 0, line 78 of boost/random/uniform_int_distribution.hpp (boost 1.55)
+#pragma warning(disable : 4724) // potential mod by 0, line 78 of boost/random/uniform_int_distribution.hpp (boost 1.55)
 #endif
 #include <boost/random/uniform_int_distribution.hpp>
 #if defined(_MSC_VER)
@@ -50,23 +50,23 @@ int fromHexChar(char _i) noexcept
         return _i - 'A' + 10;
     return -1;
 }
-}
+} // namespace
 
-bool libbitcoin::isHex(string const& _s) noexcept
+bool libbitcoin::isHex(string const &_s) noexcept
 {
     auto it = _s.begin();
     if (_s.compare(0, 2, "0x") == 0)
         it += 2;
-    return std::all_of(it, _s.end(), [](char c){ return fromHexChar(c) != -1; });
+    return std::all_of(it, _s.end(), [](char c) { return fromHexChar(c) != -1; });
 }
 
-std::string libbitcoin::escaped(std::string const& _s, bool _all)
+std::string libbitcoin::escaped(std::string const &_s, bool _all)
 {
     static const map<char, char> prettyEscapes{{'\r', 'r'}, {'\n', 'n'}, {'\t', 't'}, {'\v', 'v'}};
     std::string ret;
     ret.reserve(_s.size() + 2);
     ret.push_back('"');
-    for (auto i: _s)
+    for (auto i : _s)
         if (i == '"' && !_all)
             ret += "\\\"";
         else if (i == '\\' && !_all)
@@ -92,14 +92,14 @@ std::string libbitcoin::randomWord()
 {
     static std::mt19937_64 s_eng(0);
     std::string ret(boost::random::uniform_int_distribution<int>(1, 5)(s_eng), ' ');
-    char const n[] = "qwertyuiop";//asdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890";
+    char const n[] = "qwertyuiop"; //asdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890";
     boost::random::uniform_int_distribution<int> d(0, sizeof(n) - 2);
-    for (char& c: ret)
+    for (char &c : ret)
         c = n[d(s_eng)];
     return ret;
 }
 
-bytes libbitcoin::fromHex(std::string const& _s, WhenError _throw)
+bytes libbitcoin::fromHex(std::string const &_s, WhenError _throw)
 {
     unsigned s = (_s.size() >= 2 && _s[0] == '0' && _s[1] == 'x') ? 2 : 0;
     std::vector<uint8_t> ret;
@@ -129,11 +129,11 @@ bytes libbitcoin::fromHex(std::string const& _s, WhenError _throw)
     return ret;
 }
 
-bytes libbitcoin::asNibbles(bytesConstRef const& _s)
+bytes libbitcoin::asNibbles(bytesConstRef const &_s)
 {
     std::vector<uint8_t> ret;
     ret.reserve(_s.size() * 2);
-    for (auto i: _s)
+    for (auto i : _s)
     {
         ret.push_back(i / 16);
         ret.push_back(i % 16);
@@ -141,7 +141,7 @@ bytes libbitcoin::asNibbles(bytesConstRef const& _s)
     return ret;
 }
 
-std::string libbitcoin::toString(string32 const& _s)
+std::string libbitcoin::toString(string32 const &_s)
 {
     std::string ret;
     for (unsigned i = 0; i < 32 && _s[i]; ++i)
