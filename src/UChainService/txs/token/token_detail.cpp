64e7ecadd85c4b140cd66180a8ef7aa5a6572eba
yangguanglu <416841146@qq.com>
2019-04-30 18:23:47 +0800

    token_detial

--- src/UChainService/txs/token/token_detail.cpp
+++ src/UChainService/txs/token/token_detail.cpp
@@ -30,8 +30,10 @@
 
 #define TOKEN_SYMBOL_DELIMITER "."
 
-namespace libbitcoin {
-namespace chain {
+namespace libbitcoin
+{
+namespace chain
+{
 
 token_detail::token_detail()
 {
@@ -39,32 +41,31 @@ token_detail::token_detail()
 }
 
 token_detail::token_detail(
-    const std::string& symbol, uint64_t maximum_supply,
-    uint8_t decimal_number, uint8_t threshold, const std::string& issuer,
-    const std::string& address, const std::string& description):
-    symbol(symbol), maximum_supply(maximum_supply),
-    decimal_number(decimal_number),
-    secondaryissue_threshold(threshold),
-    unused2(0), unused3(0),
-    issuer(issuer), address(address), description(description)
+    const std::string &symbol, uint64_t maximum_supply,
+    uint8_t decimal_number, uint8_t threshold, const std::string &issuer,
+    const std::string &address, const std::string &description) : symbol(symbol), maximum_supply(maximum_supply),
+                                                                  decimal_number(decimal_number),
+                                                                  secondaryissue_threshold(threshold),
+                                                                  unused2(0), unused3(0),
+                                                                  issuer(issuer), address(address), description(description)
 {
 }
 
-token_detail token_detail::factory_from_data(const data_chunk& data)
+token_detail token_detail::factory_from_data(const data_chunk &data)
 {
     token_detail instance;
     instance.from_data(data);
     return instance;
 }
 
-token_detail token_detail::factory_from_data(std::istream& stream)
+token_detail token_detail::factory_from_data(std::istream &stream)
 {
     token_detail instance;
     instance.from_data(stream);
     return instance;
 }
 
-token_detail token_detail::factory_from_data(reader& source)
+token_detail token_detail::factory_from_data(reader &source)
 {
     token_detail instance;
     instance.from_data(source);
@@ -73,9 +74,7 @@ token_detail token_detail::factory_from_data(reader& source)
 
 bool token_detail::is_valid() const
 {
-    return !(symbol.empty()
-            || (maximum_supply==0 && !is_token_secondaryissue())
-            || (symbol.size() + 8 + 4 + issuer.size() + address.size() + description.size() + 4)>TOKEN_DETAIL_FIX_SIZE);
+    return !(symbol.empty() || (maximum_supply == 0 && !is_token_secondaryissue()) || (symbol.size() + 8 + 4 + issuer.size() + address.size() + description.size() + 4) > TOKEN_DETAIL_FIX_SIZE);
 }
 
 void token_detail::reset()
@@ -91,19 +90,19 @@ void token_detail::reset()
     description = "";
 }
 
-bool token_detail::from_data(const data_chunk& data)
+bool token_detail::from_data(const data_chunk &data)
 {
     data_source istream(data);
     return from_data(istream);
 }
 
-bool token_detail::from_data(std::istream& stream)
+bool token_detail::from_data(std::istream &stream)
 {
     istream_reader source(stream);
     return from_data(source);
 }
 
-bool token_detail::from_data(reader& source)
+bool token_detail::from_data(reader &source)
 {
     reset();
 
@@ -114,8 +113,8 @@ bool token_detail::from_data(reader& source)
     unused2 = source.read_byte();
     unused3 = source.read_byte();
     issuer = source.read_string();
-    address =  source.read_string();
-    description =  source.read_string();
+    address = source.read_string();
+    description = source.read_string();
 
     auto result = static_cast<bool>(source);
     if (!result)
@@ -133,13 +132,13 @@ data_chunk token_detail::to_data() const
     return data;
 }
 
-void token_detail::to_data(std::ostream& stream) const
+void token_detail::to_data(std::ostream &stream) const
 {
     ostream_writer sink(stream);
     to_data(sink);
 }
 
-void token_detail::to_data(writer& sink) const
+void token_detail::to_data(writer &sink) const
 {
     sink.write_string(symbol);
     sink.write_8_bytes_little_endian(maximum_supply);
@@ -163,30 +162,30 @@ std::string token_detail::to_string() const
     std::ostringstream ss;
 
     ss << "\t symbol = " << symbol << "\n"
-        << "\t maximum_supply = " << std::to_string(maximum_supply) << "\n"
-        << "\t decimal_number = " << std::to_string(decimal_number) << "\n"
-        << "\t is_token_secondaryissue = " << (is_token_secondaryissue() ? "true" : "false") << "\n"
-        << "\t secondaryissue_threshold = " << std::to_string(get_secondaryissue_threshold()) << "\n"
-        << "\t issuer = " << issuer << "\n"
-        << "\t address = " << address << "\n"
-        << "\t description = " << description << "\n";
+       << "\t maximum_supply = " << std::to_string(maximum_supply) << "\n"
+       << "\t decimal_number = " << std::to_string(decimal_number) << "\n"
+       << "\t is_token_secondaryissue = " << (is_token_secondaryissue() ? "true" : "false") << "\n"
+       << "\t secondaryissue_threshold = " << std::to_string(get_secondaryissue_threshold()) << "\n"
+       << "\t issuer = " << issuer << "\n"
+       << "\t address = " << address << "\n"
+       << "\t description = " << description << "\n";
 
     return ss.str();
 }
 
-bool token_detail::operator< (const token_detail& other) const
+bool token_detail::operator<(const token_detail &other) const
 {
     typedef std::tuple<std::string, std::string> cmp_tuple;
     return cmp_tuple(symbol, issuer) < cmp_tuple(other.symbol, other.issuer);
 }
 
-const std::string& token_detail::get_symbol() const
+const std::string &token_detail::get_symbol() const
 {
     return symbol;
 }
-void token_detail::set_symbol(const std::string& symbol)
+void token_detail::set_symbol(const std::string &symbol)
 {
-    size_t len = symbol.size()+1 < (TOKEN_DETAIL_SYMBOL_FIX_SIZE) ?symbol.size()+1:TOKEN_DETAIL_SYMBOL_FIX_SIZE;
+    size_t len = symbol.size() + 1 < (TOKEN_DETAIL_SYMBOL_FIX_SIZE) ? symbol.size() + 1 : TOKEN_DETAIL_SYMBOL_FIX_SIZE;
     this->symbol = symbol.substr(0, len);
 }
 
@@ -196,7 +195,7 @@ uint64_t token_detail::get_maximum_supply() const
 }
 void token_detail::set_maximum_supply(uint64_t maximum_supply)
 {
-     this->maximum_supply = maximum_supply;
+    this->maximum_supply = maximum_supply;
 }
 
 uint8_t token_detail::get_decimal_number() const
@@ -205,43 +204,44 @@ uint8_t token_detail::get_decimal_number() const
 }
 void token_detail::set_decimal_number(uint8_t decimal_number)
 {
-     this->decimal_number = decimal_number;
+    this->decimal_number = decimal_number;
 }
 
-const std::string& token_detail::get_issuer() const
+const std::string &token_detail::get_issuer() const
 {
     return issuer;
 }
-void token_detail::set_issuer(const std::string& issuer)
+void token_detail::set_issuer(const std::string &issuer)
 {
-     size_t len = issuer.size()+1 < (TOKEN_DETAIL_ISSUER_FIX_SIZE) ?issuer.size()+1:TOKEN_DETAIL_ISSUER_FIX_SIZE;
-     this->issuer = issuer.substr(0, len);
+    size_t len = issuer.size() + 1 < (TOKEN_DETAIL_ISSUER_FIX_SIZE) ? issuer.size() + 1 : TOKEN_DETAIL_ISSUER_FIX_SIZE;
+    this->issuer = issuer.substr(0, len);
 }
 
-const std::string& token_detail::get_address() const
+const std::string &token_detail::get_address() const
 {
     return address;
 }
-void token_detail::set_address(const std::string& address)
+void token_detail::set_address(const std::string &address)
 {
-     size_t len = address.size()+1 < (TOKEN_DETAIL_ADDRESS_FIX_SIZE) ?address.size()+1:TOKEN_DETAIL_ADDRESS_FIX_SIZE;
-     this->address = address.substr(0, len);
+    size_t len = address.size() + 1 < (TOKEN_DETAIL_ADDRESS_FIX_SIZE) ? address.size() + 1 : TOKEN_DETAIL_ADDRESS_FIX_SIZE;
+    this->address = address.substr(0, len);
 }
 
-const std::string& token_detail::get_description() const
+const std::string &token_detail::get_description() const
 {
     return description;
 }
-void token_detail::set_description(const std::string& description)
+void token_detail::set_description(const std::string &description)
 {
-     size_t len = description.size()+1 < (TOKEN_DETAIL_DESCRIPTION_FIX_SIZE) ?description.size()+1:TOKEN_DETAIL_DESCRIPTION_FIX_SIZE;
-     this->description = description.substr(0, len);
+    size_t len = description.size() + 1 < (TOKEN_DETAIL_DESCRIPTION_FIX_SIZE) ? description.size() + 1 : TOKEN_DETAIL_DESCRIPTION_FIX_SIZE;
+    this->description = description.substr(0, len);
 }
 
 std::vector<token_cert_type> token_detail::get_token_cert_mask() const
 {
     std::vector<token_cert_type> certs;
-    if (is_secondaryissue_legal()) {
+    if (is_secondaryissue_legal())
+    {
         certs.push_back(token_cert_ns::issue);
     }
 
@@ -255,7 +255,8 @@ bool token_detail::is_token_secondaryissue() const
 
 void token_detail::set_token_secondaryissue()
 {
-    if (!is_token_secondaryissue()) {
+    if (!is_token_secondaryissue())
+    {
         secondaryissue_threshold += 128;
     }
 }
@@ -311,8 +312,8 @@ bool token_detail::is_secondaryissue_owns_enough(uint64_t own, uint64_t total, u
     if (!is_secondaryissue_legal(threshold))
         return false;
     uint64_t value_needed = (uint64_t)(((double)total) / 100 * threshold);
-    return (value_needed == 0) || (own >= value_needed -1); // allow 1 inaccurate
+    return (value_needed == 0) || (own >= value_needed - 1); // allow 1 inaccurate
 }
 
-} // namspace chain
-} // namspace libbitcoin
+} // namespace chain
+} // namespace libbitcoin
