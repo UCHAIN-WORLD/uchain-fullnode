d23c280f6dc4c63c108943ce89afaee388750da3
yangguanglu <416841146@qq.com>
2019-07-05 21:40:05 +0800

    worker

--- include/UChainApp/ucd/workers/notification_worker.hpp
+++ include/UChainApp/ucd/workers/notification_worker.hpp
@@ -98,7 +98,7 @@ class BCS_API notification_worker
     bool handle_blockchain_reorganization(const code &ec, uint64_t fork_point,
                                           const block_list &new_blocks, const block_list &);
     bool handle_tx_pool(const code &ec, const index_list &,
-                                 bc::message::transaction_message::ptr tx);
+                                 bc::message::tx_message::ptr tx);
     bool handle_inventory(const code &ec,
                           const bc::message::inventory::ptr packet);
 
