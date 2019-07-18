7d1e5cb72b8c442e2b359571146a12e162506d43
yangguanglu <416841146@qq.com>
2019-07-18 19:51:41 +0800

    session outbound

--- src/UChain/node/sessions/session_outbound.cpp
+++ src/UChain/node/sessions/session_outbound.cpp
@@ -24,8 +24,8 @@
 #include <UChain/network.hpp>
 #include <UChain/node/protocols/protocol_block_in.hpp>
 #include <UChain/node/protocols/protocol_block_out.hpp>
-#include <UChain/node/protocols/protocol_transaction_in.hpp>
-#include <UChain/node/protocols/protocol_transaction_out.hpp>
+#include <UChain/node/protocols/protocol_tx_in.hpp>
+#include <UChain/node/protocols/protocol_tx_out.hpp>
 #include <UChain/node/protocols/protocol_miner.hpp>
 
 namespace libbitcoin
@@ -58,8 +58,8 @@ void session_outbound::attach_handshake_protocols(channel::ptr channel,
             auto pt_address = attach<protocol_address>(channel)->do_subscribe();
             auto pt_block_in = attach<protocol_block_in>(channel, blockchain_)->do_subscribe();
             auto pt_block_out = attach<protocol_block_out>(channel, blockchain_)->do_subscribe();
-            auto pt_tx_in = attach<protocol_transaction_in>(channel, blockchain_, pool_)->do_subscribe();
-            auto pt_tx_out = attach<protocol_transaction_out>(channel, blockchain_, pool_)->do_subscribe();
+            auto pt_tx_in = attach<protocol_tx_in>(channel, blockchain_, pool_)->do_subscribe();
+            auto pt_tx_out = attach<protocol_tx_out>(channel, blockchain_, pool_)->do_subscribe();
             channel->set_protocol_start_handler([pt_ping, pt_address, pt_block_in, pt_block_out, pt_tx_in, pt_tx_out]() {
                 pt_ping->start();
                 pt_address->start();
