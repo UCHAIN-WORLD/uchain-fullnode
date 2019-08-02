4b5b46e8ec1ccb0910afe04763e64164e213588f
yangguanglu <416841146@qq.com>
2019-08-03 06:37:52 +0800

    stealth database

--- src/UChain/database/databases/stealth_database.cpp
+++ src/UChain/database/databases/stealth_database.cpp
@@ -18,7 +18,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-#include <UChain/database/databases/stealth_database.hpp>
+#include <UChain/database/databases/stealth_db.hpp>
 
 #include <cstddef>
 #include <cstdint>
