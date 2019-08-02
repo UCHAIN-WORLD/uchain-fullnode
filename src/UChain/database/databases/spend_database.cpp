8e271ff9b65303549058787b2334a84c65801db6
yangguanglu <416841146@qq.com>
2019-08-03 06:37:25 +0800

    spend database

--- src/UChain/database/databases/spend_database.cpp
+++ src/UChain/database/databases/spend_database.cpp
@@ -18,7 +18,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-#include <UChain/database/databases/spend_database.hpp>
+#include <UChain/database/databases/spend_db.hpp>
 
 #include <algorithm>
 #include <cstddef>
